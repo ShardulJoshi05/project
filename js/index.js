47b98d916bd49c795157a81560ecf05645344dd1
ShardulJoshi05 <shardulj.nmims@gmail.com>
2022-12-22 21:20:37 +0530

    Req : 1,2,3,4,5 Check : 1 is completed

--- js/index.js
+++ js/index.js
@@ -6,15 +6,23 @@ function startTimer() {
     var startObj = new Date(startDate + ' ' + startTime);
     var endObj = new Date(endDate + ' ' + endTime);
 
-    function x(){
-        var timer = setInterval(function () {
-            startObj = addSeconds(startObj, 1);
-            if (calculateDifference(startObj, endObj) == 0) {
-                clearInterval(timer);
-            }
-        }, 1000);
+
+    if (startDate == '' || startTime == '' || endDate == '' || endTime == '') {
+        var msg = 'Please fill all the fields';
+        document.getElementById('message').innerText = msg;
+
+        return;
+    } else {
+        function x() {
+            var timer = setInterval(function () {
+                startObj = addSeconds(startObj, 1);
+                if (calculateDifference(startObj, endObj) == 0) {
+                    clearInterval(timer);
+                }
+            }, 1000);
+        }
+        x();
     }
-    x();
 }
 
 function calculateDifference(start,end) {
@@ -31,7 +39,11 @@ function calculateDifference(start,end) {
         minutes: Math.floor(difference / 60) % 60,
         seconds : Math.floor(difference)%60
     }
-    console.log(differenceObj);
+
+    document.getElementById('days').innerText = differenceObj.days;
+    document.getElementById('hours').innerText = differenceObj.hours;
+    document.getElementById('minutes').innerText = differenceObj.minutes;
+    document.getElementById('seconds').innerText = differenceObj.seconds;
 }
 
 function addSeconds(sdate, seconds) {
